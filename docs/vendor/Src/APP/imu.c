c7d01203b13215f71155a54b9d6ca8c1e37a92de
Rpid <K13640213395@outlook.com>
2026-03-18 14:57:51 +0800

    remove vendor code from git tracking

--- docs/vendor/Src/APP/imu.c
+++
@@ -1,9 +0,0 @@
-#include "APP/imu.h"
-
-const float gyro_dps2rad = 2048.0f/32768.0f*3.14159f/180.0f;
-const float accel_dps2grav = 16.0f/32768.0f;
-const float accel_dps2accel = 9.8f*16.0f/32768.0f;
-Imu_t xImu;
-
-
-
